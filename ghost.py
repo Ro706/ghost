d748d1f211bf59a460accb1fcc7cfb50e1fed8a1
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-02-27 18:15:51 +0530

    Update ghost_game

--- ghost.py
+++ ghost.py
@@ -1,8 +1,9 @@
-# Cades ghost game, created by python_games and DK community
-#Just paste this into a python IDLE window, and tweak if you are professinal!
-from random import randint
+random import randint
 from time import sleep
-
+from pyfiglet import Figlet
+from termcolor import colored
+f=Figlet(font='slant')
+print (f.renderText('Ghost_Game'))
 print("Cades ghost game!")
 feelingbrave = True
 score = 0
@@ -15,14 +16,18 @@ while feelingbrave:
     print("You have to open one, which one?")
     door = input('1, 2, or 3?')
     doornum = int(door)
-    if doornum == ghostdoor:
-        print("GHOST!!!")
-        feelingbrave = False
+    if doornum == 1 or doornum == 2 or doornum == 3:                                                         if doornum == ghostdoor:
+            print(colored("GHOST!!!",'red'))
+            feelingbrave = False
+        else:
+            print("No ghost")
+            sleep(0.5)
+            print(colored("Go to next level!",'green'))
+            score = score + 1
     else:
-        print("No ghost")
-        sleep(0.5)
-        print("Go to next level!")
-        score = score + 1
+        print (colored('bye','red))
+        break
 print("You ran away!")
 sleep(1)
 print('Game over! you scored', score, "!")
+print('made by Ro706')
