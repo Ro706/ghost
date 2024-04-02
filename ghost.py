51e82e66f3e89dc21f91bc23ec447b5ff11da75e
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2024-04-03 01:37:04 +0530

    Update ghost.py

--- ghost.py
+++ ghost.py
@@ -2,11 +2,13 @@ from random import randint
 from time import sleep
 from pyfiglet import Figlet
 from termcolor import colored
-f=Figlet(font='slant')
-print (f.renderText('Ghost_Game'))
-print("Cades ghost game!")
+
+f = Figlet(font='slant')
+print(f.renderText('Ghost_Game'))
+print("Welcome to Ghost Game!")
 feelingbrave = True
 score = 0
+
 while feelingbrave:
     ghostdoor = randint(1, 3)
     print("Three doors are ahead...")
@@ -14,20 +16,19 @@ while feelingbrave:
     print("One has a ghost in it!!!")
     sleep(0.5)
     print("You have to open one, which one?")
-    door = input('1, 2, or 3?')
+    door = input('1, 2, or 3? ')
     doornum = int(door)
-    if doornum == 1 or doornum == 2 or doornum == 3:           
-            print(colored("GHOST!!!",'red'))
-            feelingbrave = False
-        else:
-            print("No ghost")
-            sleep(0.5)
-            print(colored("Go to next level!",'green'))
-            score = score + 1
+
+    if doornum == ghostdoor:
+        print(colored("GHOST!!!", 'red'))
+        feelingbrave = False
     else:
-        print (colored('bye','red))
-        break
+        print("No ghost")
+        sleep(0.5)
+        print(colored("Go to the next level!", 'green'))
+        score += 1
+
 print("You ran away!")
 sleep(1)
-print('Game over! you scored', score, "!")
-print('made by Ro706')
+print('Game over! You scored', score, "!")
+print('Made by Ro706')
